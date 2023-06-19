63d3e77b070f04484ebf5796f129551fbd1b20ea
ecemozturkk <ecemozturk704@gmail.com>
2023-06-19 13:25:09 +0300

    Removed unnecessary code for font

--- MoviesApp/AppDelegate.swift
+++ MoviesApp/AppDelegate.swift
@@ -14,12 +14,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        
-        UIFont.familyNames.forEach({name in
-            for font_name in UIFont.fontNames(forFamilyName: name) {
-                print("\n\(font_name)")
-            }
-        })
         return true
     }
 
