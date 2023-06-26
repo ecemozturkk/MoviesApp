0f25a5d72a58b51f9a955cbf095f6f138bef8800
ecemozturkk <ecemozturk704@gmail.com>
2023-06-27 00:32:25 +0300

    Added clean swift template

---
+++ MoviesApp/CleanModels.swift
@@ -0,0 +1,30 @@
+//
+//  CleanModels.swift
+//  MoviesApp
+//
+//  Created by Ecem Öztürk on 26.06.2023.
+//
+
+import Foundation
+
+// swiftlint:disable nesting
+enum Clean {
+    
+    enum Case {
+        
+        struct Request {
+            
+        }
+        
+        struct Response {
+            
+        }
+        
+        struct ViewModel {
+            
+        }
+        
+    }
+    
+}
+// swiftlint:enable nesting
