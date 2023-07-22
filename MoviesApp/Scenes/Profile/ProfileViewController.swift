b852b46c268af325e5c96f2e58984093471f70ca
ecemozturkk <ecemozturk704@gmail.com>
2023-07-22 23:22:50 +0300

    WebKit View added

--- MoviesApp/Scenes/Profile/ProfileViewController.swift
+++ MoviesApp/Scenes/Profile/ProfileViewController.swift
@@ -13,9 +13,33 @@ class ProfileViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
 
     
+    private let button: UIButton = {
+        let button = UIButton()
+        button.setTitle("LinkedIn", for: .normal)
+        button.backgroundColor = .link
+        button.setTitleColor(.white, for: .normal)
+        return button
+        
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.addSubview(button)
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+        button.frame = CGRect(x: 0, y: 0, width: 220, height: 50)
+        button.center = view.center
 
+        
+    }
+    
+    @objc private func didTapButton() {
+        guard let url = URL(string: "https://www.linkedin.com/in/ecemozturk704/") else {
+            return
+        }
+        let vc = WebViewController(url: url, title: "LinkedIn")
+        let navVC = UINavigationController(rootViewController: vc)
+        present(navVC, animated: true)
         
     }
     
